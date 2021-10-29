6d957f8351285b3d9271b2a085146f3cc72cd8b2
reduckted <reduckted@outlook.com>
2021-10-29 20:14:25 +1000

    1.3.0

--- source/Emphasize/Properties/AssemblyInfo.cs
+++ source/Emphasize/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Emphasize")]
 [assembly: AssemblyProduct("Emphasize")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.2.3.0")]
-[assembly: AssemblyFileVersion("1.2.3.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
